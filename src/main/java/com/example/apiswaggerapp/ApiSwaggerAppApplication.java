91b4c7c00579d2f6c8823d171710b7ccc4025f39
SlawomirWenc <slawomirwenc@gmail.com>
2021-04-28 20:03:18 +0200

    Initial commit

---
+++ src/main/java/com/example/apiswaggerapp/ApiSwaggerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiswaggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSwaggerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSwaggerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiswaggerapp/ApiSwaggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiswaggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSwaggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
